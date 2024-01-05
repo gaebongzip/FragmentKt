a30bc0429077a932f30f3b5372fa4826787657ce
gaebongzip <gaebongzip@gmail.com>
2024-01-05 16:33:01 +0900

    first commit

---
+++ app/src/main/java/com/example/fragmentkt/Fragment1.kt
@@ -0,0 +1,27 @@
+package com.example.fragmentkt
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentkt.databinding.Frag1Binding
+
+class Fragment1 : Fragment() {
+
+    private var mBinding : Frag1Binding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        //val view = inflater.inflate(R.layout.frag1, container, false)
+        mBinding = Frag1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
+    }
+}
